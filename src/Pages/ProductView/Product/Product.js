818b5f3a9036584fb89d16f364348b861b4cbcf2
MadhanMurali <madhanmurali53@gmail.com>
2020-08-23 15:09:18 +0530

    product display half complete: need to implement image zoom

--- src/Pages/ProductView/Product/Product.js
+++ src/Pages/ProductView/Product/Product.js
@@ -14,8 +14,9 @@ import ProductImage8 from '../../../assets/img/product/ProductSample8.jpg';
 
 const slideDown = (event, productIconCount, currentProductIconCount, setCurrentProductIconCount) => {
     event.preventDefault();
-
+    
     //3 : offset to display icons, 72: height of icon (68) and margin (4)
+    // to move 2 icons within single click, change 3 to 2 and 72 (from second) to 144
     const maxTranslateY = (productIconCount - 3) * 72;
     const currentTranslateY = (currentProductIconCount - 1) * 72;
     if (maxTranslateY === currentTranslateY) {
@@ -67,8 +68,8 @@ const Product = () => {
 
         const productCarouselOptions = {
             fullWidth: true,
-            indicators: false,
-            duration: 50,
+            indicators: true,
+            duration: 100,
         };
 
         const variantElem = document.getElementById('variant-select');
@@ -90,75 +91,68 @@ const Product = () => {
 
     return (
         <div className = "row card-panel">
-            <div className="col s12 m12 l6 hide-on-small-only">
-                <div className="col s9 m9 l9">
-                    <div className="">
-                        <div id="product-carousel" className="carousel carousel-slider center product-photo-focussed">
-                            <a className="carousel-item" href="#!"><img src={ProductImage} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage2} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage3} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage4} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage5} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage6} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage7} alt="product"></img></a>
-                            <a className="carousel-item" href="#!"><img src={ProductImage8} alt="product"></img></a>
-                        </div>
-                    </div>
+            <div className="row">
+                <div className="col s12 m12 l1"></div>
+                <div className="col s12 m12 l10">
+                    <h6>Product Name is Lorem Ipsum</h6>
                 </div>
-                <div className="col s3 m3 l3">
+                <div className="col s12 m12 l1"></div>
+            </div>
+            <div className="col s12 m12 l6 valign-wrapper">
+                <div className="col s12 m3 l3 card-panel hide-on-small-only">
 
-                    <div className="product-photo-collection-prev-btn-div">
-                        <p className="product-photo-collection-prev-para">
-                            <a className="product-photo-collection-prev-btn" href="#!" onClick={e => slideUp(e, productIconCount, currentProductIconCount, setCurrentProductIconCount)}>
+                    <div className="z-depth-1 product-photo-collection-prev-btn-div">
+                        <a className="product-photo-collection-prev-btn" href="#!" onClick={e => slideUp(e, productIconCount, currentProductIconCount, setCurrentProductIconCount)}>
+                            <p className="product-photo-collection-prev-para">
                                 <i className="material-icons">expand_less</i>
-                            </a>
-                        </p>
+                            </p>
+                        </a>
                     </div>
                     
                     <div className="product-photo-collection">                        
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 0)} >
                                 <img src={ProductImage} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 1)} >
                                 <img src={ProductImage2} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 2)} >
                                 <img src={ProductImage3} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 3)} >
                                 <img src={ProductImage4} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 4)} >
                                 <img src={ProductImage5} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 5)} >
                                 <img src={ProductImage6} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 6)} >
                                 <img src={ProductImage7} alt="product"></img>
                             </a>
                         </div>
-                        <div className="product-photo-collection-icon-div">
+                        <div className="z-depth-1 product-photo-collection-icon-div">
                             <a className="product-photo-collection-icon" href="#!"
                                 onClick={ e => moveToProductImage(e, productCarouselInstance, 7)} >
                                 <img src={ProductImage8} alt="product"></img>
@@ -166,21 +160,93 @@ const Product = () => {
                         </div>
                     </div>
 
-                    <div className="product-photo-collection-next-btn-div">
-                        <p className="product-photo-collection-next-para">
-                            <a className="product-photo-collection-next-btn" href="#!" onClick={e => slideDown(e, productIconCount, currentProductIconCount, setCurrentProductIconCount)}>
+                    <div className="z-depth-1 product-photo-collection-next-btn-div">
+                        <a className="product-photo-collection-next-btn" href="#!" onClick={e => slideDown(e, productIconCount, currentProductIconCount, setCurrentProductIconCount)}>
+                            <p className="product-photo-collection-next-para">
                                 <i className="material-icons">expand_more</i>
+                            </p>
+                        </a>                        
+                    </div>
+                    
+                </div>
+
+                <div className="col s12 m6 l6">
+                    <div id="product-carousel" className="z-depth-3 carousel carousel-slider center product-photo-focussed">
+                        <a className="carousel-item" href="#!"><img src={ProductImage} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage2} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage3} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage4} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage5} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage6} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage7} alt="product"></img></a>
+                        <a className="carousel-item" href="#!"><img src={ProductImage8} alt="product"></img></a>
+                    </div>
+                </div>
+
+                <div className="col s3 m3 l3"></div>
+
+                {/** small screen product icons -- incomplete and it may not be required for small screens */}
+
+                {/* <div className="col s12 m3 l3 hide-on-med-and-up">
+                    <div className="valign-wrapper">
+                        <p className="center-align product-photo-collection-prev-para-small-screen">
+                            <a className="" href="#!" onClick={e => slideUp(e, productIconCount, currentProductIconCount, setCurrentProductIconCount)}>
+                                <i className="material-icons">chevron_left</i>
+                            </a>
+                        </p>
+                        <div className="product-photo-collection-small-screen-column ">
+                            <div className="product-photo-collection-small-screen-div">
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 0)} >
+                                    <img src={ProductImage} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 1)} >
+                                    <img src={ProductImage2} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 2)} >
+                                    <img src={ProductImage3} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 3)} >
+                                    <img src={ProductImage4} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 4)} >
+                                    <img src={ProductImage5} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 5)} >
+                                    <img src={ProductImage6} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 6)} >
+                                    <img src={ProductImage7} alt="product"></img>
+                                </a>
+                                <a className="product-photo-collection-icon-small-screen" href="#!"
+                                    onClick={ e => moveToProductImage(e, productCarouselInstance, 7)} >
+                                    <img src={ProductImage8} alt="product"></img>
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <p className="center-align product-photo-collection-next-para-small-screen">
+                            <a href="#!" onClick={e => slideDown(e, productIconCount, currentProductIconCount, setCurrentProductIconCount)}>
+                                <i className="material-icons">chevron_right</i>
                             </a>
                         </p>
                     </div>
 
-                </div>
+                </div> */}
+
+
             </div>
+            
             <div className="col s12 m12 l6">
                 <div className="row">
-                    <div className="col s12">
                         <div className="col s12">
-                            <p><span className="product-pricing red-text text-lighten-2">Rs. 2000</span></p>
+                            <p className="product-pricing red-text text-lighten-2">Rs. 2000</p>
                         </div>
                         <form className="col s12">
                             <div className="row">
@@ -212,7 +278,6 @@ const Product = () => {
                                 </button>
                             </div>
                         </form>
-                    </div>
                 </div>
             </div>
         </div>

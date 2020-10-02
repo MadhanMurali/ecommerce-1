33cf5298b7c11a887ed90979a6c9bd28acabdab2
MadhanMurali <madhanmurali53@gmail.com>
2020-10-02 22:54:37 +0530

    replaced google sign out button with normal button in side-menu

--- src/PageBlocks/Header/SmallScreenSideMenu/SmallScreenSideMenu.js
+++ src/PageBlocks/Header/SmallScreenSideMenu/SmallScreenSideMenu.js
@@ -37,8 +37,11 @@ const SmallScreenSideMenu = ({menu_button_color_class, title_color_class, option
                             : <img className="center-profile-image" src = {user.image_address} alt=""/>;
 
     const signOutButton = (user.social_id_type === "google")
-                            ? <GoogleLogout button_color_class="white" 
-                                other_classes="black-text" title='Sign Out'/>
+                            ? <GoogleLogout 
+                                button_color_class="teal darken-3" 
+                                icon_name="exit_to_app"
+                                icon_color_class="white-text"
+                                title='Sign Out'/>
                             : <NormalAccountLogout 
                                 button_color_class={options_button_color_class} icon_name="exit_to_app" 
                                 title="Sign Out" icon_color_class="white-text"/>;
